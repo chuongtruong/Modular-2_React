b5d07eb9f68cbfb7f164dc9a55e11b39f94efcce
Chuong Truong <chuongtruong290893@gmail.com>
2021-01-26 01:14:21 -0800

    change MenuComponent and DishdetailCompenent to functional component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,82 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+            <CardImgOverlay body className="ml-5">
+                <CardTitle>
+                    {dish.name}
+                </CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    renderComments(c) {
-        const comment = c.map((cmt) => {
-            return (
-                <div key={cmt.id}>
-                    <h4>{cmt.comment}</h4>
-                    <p>-- {cmt.author}, {cmt.date}</p>
-                </div>
-            );
-        });
-        return comment
-    }
-    
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
-                            <CardBody>
-                                <CardTitle>
-                                    {dish.name}
-                                </CardTitle>
-                                <CardText>
-                                    {dish.description}
-
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-
-                    <div className="col-12 col-md-5 m-1">
-                        <h3>Comments</h3>
-                        {this.renderComments(dish.comments)}
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-
-                </div>
-            )
-        }
-    }
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={()=>this.props.onClick(dish.id)}>
-                        <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
-                        <CardImgOverlay body className="ml-5">
-                            <CardTitle>
-                                {dish.name}
-                            </CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+//another way to declare a functional component
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} onClick={props.onClick}></RenderMenuItem>
             </div>
         );
-    }
+    });
+    
+    return (
+        <div className="container" onClick={() => { console.log("clicked !!") }}>
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
+
 
 export default Menu;
