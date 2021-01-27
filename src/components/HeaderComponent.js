7c6debcf11bcb6b6be1422fc0d8f02f65a0adbe3
Chuong Truong <chuongtruong290893@gmail.com>
2021-01-26 20:49:40 -0800

    router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false
+    }
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  //Defining a function, see the <NavbarToggler onClick={this.toggleNav} /> below ?, but in order to use this function, we ALSO have
+  //to bind it to the component, in the contructor ^^/ A little hard to understand.
+  //see videos https://www.coursera.org/learn/front-end-react/lecture/meLZh/exercise-video-react-router
+  toggleNav(){
+    this.setState({ 
+      isNavOpen: !this.state.isNavOpen
+    })
+  }
+
   render() {
-    return(
-    <React.Fragment>
-      <Navbar dark>
-        <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Jumbotron>
-           <div className="container">
-               <div className="row row-header">
-                   <div className="col-12 col-sm-6">
-                       <h1>Ristorante con Fusion</h1>
-                       <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                   </div>
-               </div>
-           </div>
-       </Jumbotron>
-    </React.Fragment>
+    return (
+      <React.Fragment>
+        <Navbar dark expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img src="assets/images/logo.png" height="30" width="41" />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home"><span className="fa fa-home fa-lg">Home</span></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus"><span className="fa fa-info fa-lg">About Us</span></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/menu"><span className="fa fa-list fa-lg">Menu</span></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus"><span className="fa fa-address-card fa-lg">Contact Us</span></NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Ristorante con Fusion</h1>
+                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </React.Fragment>
     );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent'
-import DishDetail from './DishdetailComponent';
-import { DISHES } from "../shared/dishes";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from "../shared/dishes";
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId })
-    }
-
     render() {
+        const HomePage = () => {
+            return(
+                <Home/>
+            );
+        }
         return (
             <div className="App">
                 <Header/>
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+
+                {/* Before without routing */}
+                {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+                {/* With routing */}
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes = {this.state.dishes}/>} />
+                    <Redirect  to="/home" />
+                </Switch>
                 <Footer/>
             </div>
         );
